e72252c4ed5fe3e6d9819c5970d3c2c0d66b1c81
Joey Haalboom <joey@eskesmedia.nl>
2023-02-10 16:24:40 +0100

    added args to command for download from other programs

--- download.py
+++ download.py
@@ -2,97 +2,64 @@ from sclib import SoundcloudAPI, Track, Playlist
 import sys
 from pytube import YouTube
 import os
-from sclib.sync import UnsupportedFormatError
-import re
+
 music_type = {
-    "DRUMBASS" : "C:\\Users\\jhaal\\Music\\Feestje\\drumbass\\",
-    "TECHNO" : "C:\\Users\\jhaal\\Music\\Feestje\\techno\\",
-    "REST" : "C:\\Users\\jhaal\\Music\\Feestje\\rest\\",
-    "REQUEST" : "C:\\Users\\jhaal\\Music\\Feestje\\request\\"
+  "drumbass" : "/mnt/h/Mijn Drive/Muziek/drumbass",
+  "techno" : "/mnt/h/Mijn Drive/Muziek/techno",
+  "rest" : "/mnt/h/Mijn Drive/Muziek/rest",
+  "request" : "/mnt/h/Mijn Drive/Muziek/request"
 }
 
-print("Enter DRUMBASS/TECHNO/REST/REQUEST")
-FOLDER = str(input())
+FOLDER = str(sys.argv[1])
 bestand_locatie = music_type[FOLDER]
-print(bestand_locatie)
+thres = 1
 
+url = sys.argv[2]
 
-print("Enter file threshold (leave blank for 1mb)")
-thres = str(input())
-thres = int(thres) if len(thres) > 0 else 1
+if "soundcloud" in url:
+  if "playlist+:" in url:
+    api = SoundcloudAPI()
+    playlist_url = url.split('+:')[-1]
+    playlist = api.resolve(playlist_url)
+    assert type(playlist) is Playlist
+    num = len(playlist.tracks)
+    i = 0
+    for track in playlist.tracks:
+      i += 1
+      print(f"Track {i} / {num}")
+      filename = f'./{track.artist} - {track.title}.mp3'
+      with open(bestand_locatie + filename, 'wb+') as fp:
+        track.write_mp3_to(fp)
+        print(f"\t{filename} downloaded.")
+        
+  else:
+    api = SoundcloudAPI()
+    track = api.resolve(url)
+    assert type(track) is Track
+    filename = f'{track.artist} - {track.title}.mp3'
+    with open(bestand_locatie + filename, 'wb+') as fp:
+        track.write_mp3_to(fp)
+    
+    file_size = os.path.getsize(bestand_locatie + filename) / 1000000
 
-while True:
-    # url input from user
-    url = str(input("Enter the URL of the video you want to download: \n>> "))
-    print(f"This is the {FOLDER} prompt!")
+    if file_size < thres:
+        print(f"Threshold is not met -> file deleted. Try another version.")
+        os.remove(bestand_locatie + filename)
+    else:
+        print(filename + "  has been successfully downloaded from Soundcloud.")
 
-    if "soundcloud" in url:
-        if "playlist+:" in url:
-            try:
-                api = SoundcloudAPI()
-                playlist_url = url.split('+:')[-1]
-                playlist = api.resolve(playlist_url)
-                print(playlist)
-                assert type(playlist) is Playlist
-                num = len(playlist.tracks)
-                i = 0
-                for track in playlist.tracks:
-                    i += 1
-                    print(f"Track {i} / {num}")
-                    try:
-                        filename = f'./{track.artist} - {track.title}.mp3'
-                        with open(bestand_locatie + filename, 'wb+') as fp:
-                            track.write_mp3_to(fp)
-                        print(f"\t{filename} downloaded.")
-                        print(f"This is the {FOLDER} prompt!")
-                    except (UnsupportedFormatError, FileNotFoundError, OSError) as e:
-                        print(f"\t{filename} -> This file is not supported.")
-                        continue
+elif "youtube" in url:
+  yt = YouTube(url)
 
-            except UnsupportedFormatError:
-                continue
-        else:
-            api = SoundcloudAPI()
+  video = yt.streams.filter(only_audio=True).first()
 
-            track = api.resolve(url)
+  out_file = video.download(output_path=bestand_locatie)
+  base, ext = os.path.splitext(out_file)
+  new_file = base + '.mp3'
+  os.rename(out_file, new_file)
 
-            assert type(track) is Track
-
-            filename = f'{track.artist} - {track.title}.mp3'
-
-            with open(bestand_locatie + filename, 'wb+') as fp:
-                track.write_mp3_to(fp)
-            
-            file_size = os.path.getsize(bestand_locatie + filename) / 1000000
-
-            if file_size < thres:
-                print(f"Threshold is not met -> file deleted. Try another version.")
-                os.remove(bestand_locatie + filename)
-            else:
-                print(filename + "  has been successfully downloaded from Soundcloud.")
-
-    elif "youtube" in url:
-        try:
-            yt = YouTube(url)
-
-            # extract only audio
-            video = yt.streams.filter(only_audio=True).first()
-
-            # download the file
-            out_file = video.download(output_path=bestand_locatie)
-            print(sys.getsizeof(yt))
-            # save the file
-            base, ext = os.path.splitext(out_file)
-            new_file = base + '.mp3'
-            os.rename(out_file, new_file)
-
-            file_size = os.path.getsize(new_file) / 1000000
-            if file_size < thres:
-                print(f"Threshold is not met -> file deleted. Try another version.")
-                os.remove(new_file)
-            # result of success
-            print(yt.title + " has been successfully downloaded from Youtube.")
-            # print("Something went wrong - try again.")
-        except FileExistsError:
-            print("File already exist -> skipped.")
-            continue
+  file_size = os.path.getsize(new_file) / 1000000
+  if file_size < thres:
+      print(f"Threshold is not met -> file deleted. Try another version.")
+      os.remove(new_file)
+  print(yt.title + " has been successfully downloaded from Youtube.")
